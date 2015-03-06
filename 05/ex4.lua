7d7d87d26cb6f618872a563d55458bf35051249e
Alien Lien <alien_lien@htc.com>
2015-03-07 02:27:40 +0800

    Begin to rewrite ex4 in chap. 5

--- 05/ex4.lua
+++ 05/ex4.lua
@@ -1,36 +1,17 @@
-function C_m(array, m)
-    n = #array
-    rest = gen_rest_array(array)
- 
+-- genCombNum returns the combinational number for (n, m)
+-- It is used as a reference to find all the combination.
+function genCombNum(n, m)
     if n < m then
-        return nil
+        return 0
     end
 
     if m == 0 then
-        return {}
+        return 1
     end
 
-    C_1 = C_m(rest, m-1)
-    C_2 = C_m(rest, m)
-
-    result = {}
-    for k, v in ipairs(C_1) do
-        result[k] = table.pack(array[1], table.unpack(C_1))
+    if n == m then
+        return 1
     end
 
-    n_1 = #C_1
-    for k, v in ipairs(C_2) do
-        result[n_1 + k] = v
-    end
-    return result
-end
-
-function gen_rest_array(array)
-    result = {}
-    for i, item in ipairs(array) do
-        if i ~= 1 then
-            result[i-1] = item
-        end
-    end
-    return result
+    return genCombNum(n-1, m-1) + genCombNum(n-1, m)
 end
